cf09fa8a461baa576a4de106322a25ab4ad68dbb
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-06-17 13:05:24 -0400

    feeling good about the anagrams one

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,7 +8,27 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let obj1 = makeObj(stringA);
+  let obj2 = makeObj(stringB);
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  for (let el in obj1) {
+    if (obj1[el] !== obj2[el]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const makeObj = (str) => {
+  let obj = {};
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    obj[char] = obj[char] + 1 || 1;
+  }
+  return obj;
+};
 
 // hint below
 //
